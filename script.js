023c8fe0df8989eeed18c1bbbbe8c2cd5ee89d70
Kadir-Saliu <kadir_saliu@hotmail.de>
2026-03-05 12:15:28 +0100

    fix the styles and text

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function getDomElements() {
     techIcons: document.querySelector(".project-icon"),
     previewImg: document.querySelector(".project-preview-img"),
     liveBtn: document.querySelector(".project-btn-live"),
-    gitBtn: document.querySelector(".project-btn-git")
+    gitBtn: document.querySelector(".project-btn-git"),
   };
 }
 
@@ -62,7 +62,7 @@ function renderSections(sections) {
           <h3 class="project-heading">${sec.title}</h3>
           <p class="project-description">${sec.text}</p>
         </div>
-      </section>`
+      </section>`,
     )
     .join("");
 }
@@ -96,66 +96,66 @@ function getProjects() {
       sections: [
         {
           title: "About the Project",
-          text: "A simple Jump-and-Run game based on an object-oriented approach. Help El Pollo Loco to find coins and poison bottles to fight against the killer whale."
+          text: "A simple Jump-and-Run game based on an object-oriented approach. Help El Pollo Loco to find coins and poison bottles to fight against the killer whale.",
         },
         {
           title: "How I have organised my work process",
-          text: " How do you keep your code clean and maintainable? Have you broken the project down into reusable modules? Focus on documentation, naming files, variables, classes and testing."
+          text: " How do you keep your code clean and maintainable? Have you broken the project down into reusable modules? Focus on documentation, naming files, variables, classes and testing.",
         },
         {
           title: "What I have learned",
-          text: "As someone who is always trying to learn new technologies, you may have enjoyed working on this project because it allowed you to delve deep into a particular technology."
-        }
-      ],
-      tech: [
-        "./assets/img/icons/tech-icons/html-project icon.png",
-        "./assets/img/icons/tech-icons/js-project-icon.png",
-        "./assets/img/icons/tech-icons/css-project-icon.png"
-      ],
-      img: "./assets/img/project-img/el-pollo-loco-2 (1).png",
-      live: "https://kadir-saliu.developerakademie.net/el_pollo_loco/index.html",
-      git: "https://github.com/Kadir-Saliu"
-    },
-
-    "join": {
-      sections: [
-        {
-          title: "About the Project",
-          text: "Duration: 2 months. Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories."
+          text: "As someone who is always trying to learn new technologies, you may have enjoyed working on this project because it allowed you to delve deep into a particular technology.",
         },
-        {
-          title: "How I have organised my work process",
-          text: "How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing."
-        },
-        {
-          title: "My group work experience",
-          text: "How many people were in the team and what was your role? Describe your tasks in 1–2 sentences. It is nice to mention a good teamwork and cooperation."
-        }
       ],
       tech: [
         "./assets/img/icons/tech-icons/html-project icon.png",
         "./assets/img/icons/tech-icons/js-project-icon.png",
         "./assets/img/icons/tech-icons/css-project-icon.png",
-        "./assets/img/icons/tech-icons/firebase-project-icon.png"
+      ],
+      img: "./assets/img/project-img/el-pollo-loco-2 (1).png",
+      live: "https://kadir-saliu.developerakademie.net/el_pollo_loco/index.html",
+      git: "https://github.com/Kadir-Saliu",
+    },
+
+    join: {
+      sections: [
+        {
+          title: "About the Project",
+          text: "Duration: 2 months. Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+        },
+        {
+          title: "How I have organised my work process",
+          text: "How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.",
+        },
+        {
+          title: "My group work experience",
+          text: "How many people were in the team and what was your role? Describe your tasks in 1–2 sentences. It is nice to mention a good teamwork and cooperation.",
+        },
+      ],
+      tech: [
+        "./assets/img/icons/tech-icons/html-project icon.png",
+        "./assets/img/icons/tech-icons/js-project-icon.png",
+        "./assets/img/icons/tech-icons/css-project-icon.png",
+        "./assets/img/icons/tech-icons/firebase-project-icon.png",
       ],
       img: "./assets/img/project-img/join.png",
       live: "https://kadir-saliu.developerakademie.net/join/index.html",
-      git: "https://github.com/Kadir-Saliu"
+      git: "https://github.com/Kadir-Saliu",
     },
 
     "ongoing-project": {
       sections: [
         {
           title: "About the project",
-          text: "What are you currently working on and what features are you implementing? What is the key to success in your projects? You can mention good planning, code quality and execution. Show a desire to learn new technologies and continually improve your skills."
-        }
+          text: "What are you currently working on and what features are you implementing? What is the key to success in your projects? You can mention good planning, code quality and execution. Show a desire to learn new technologies and continually improve your skills.",
+        },
       ],
       tech: [
         "./assets/img/icons/tech-icons/angular-project-icon.png",
         "./assets/img/icons/tech-icons/ts-project-icon.png",
-        "./assets/img/icons/tech-icons/js-project-icon.png"
+        "./assets/img/icons/tech-icons/js-project-icon.png",
       ],
-      img: "./assets/img/project-img/coming-soon.png"
-    }
+      img: "./assets/img/project-img/coming-soon.png",
+    },
   };
 }
